b074deaf887210db8c367278e1e91a954ec7c832
abhishekpaturkar <godkillop03@gmail.com>
2024-06-25 20:53:06 +0530

    types added

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,7 +1,25 @@
 import React from "react"
+import SearchBarForm from "./SearchBarForm"
 
 const SearchBar = () => {
-  return <div>SearchBar</div>
+  return (
+    <div className="flex justify-center items-center w-full p-3 bg-black/40">
+      <div className="w-1/6 flex justify-center items-center text-neutral-100">
+        <a href={"/"} className="flex justify-center items-center">
+          <img
+            src="/logo.png"
+            alt="logo"
+            className="w-10 object-cover rounded-full"
+          />
+          <div className="px-2 md:block hidden font-bold text-xl">FootyHub</div>
+        </a>
+      </div>
+      <div className="w-4/6 flex justify-center items-center">
+        <SearchBarForm />
+      </div>
+      <div className="w-1/6"></div>
+    </div>
+  )
 }
 
 export default SearchBar
